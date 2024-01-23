517729d89e31c4405fdf2889f0ab4b013e848f13
dcrescimbeni <crescimbenidino@gmail.com>
2024-01-23 08:48:04 -0300

    feat: added links to headers

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,27 +1,36 @@
 import Image from "next/image";
 import React from "react";
 import separator from "../_assets/dotted-squiggly.svg";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <>
-      <h1 className="font-serif flex flex-col text-6xl text-center text-slate-600 whitespace-pre items-center font-extralight tracking-tighter mb-4">
-        <span className="text-xl font-bold text-slate-500 -mb-2 tracking-normal">
-          the
-        </span>
-        <span className="-mb-5">DXdao</span>
-        <span>archives</span>
-      </h1>
+      <Link href="/">
+        <h1 className="font-serif flex flex-col text-6xl text-center text-slate-600 whitespace-pre items-center font-extralight tracking-tighter mb-4">
+          <span className="text-xl font-bold text-slate-500 -mb-2 tracking-normal">
+            the
+          </span>
+          <span className="-mb-5">DXdao</span>
+          <span>archives</span>
+        </h1>
+      </Link>
 
-      <h2 className="flex flex-col items-center font-serif text-center text-slate-600">
-        <span className="text-xs">by</span>
-        <span className="flex">
-          THE
-          <Image src={separator} alt="separator" className="w-20 px-2" />
-          INTERNET
-        </span>
-        <span>COMMUNITY COMPANY</span>
-      </h2>
+      <Link
+        href="https://internetcommunity.co/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h2 className="flex flex-col items-center font-serif text-center text-slate-600">
+          <span className="text-xs">by</span>
+          <span className="flex">
+            THE
+            <Image src={separator} alt="separator" className="w-20 px-2" />
+            INTERNET
+          </span>
+          <span>COMMUNITY COMPANY</span>
+        </h2>
+      </Link>
     </>
   );
 };
