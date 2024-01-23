33c63c806cf3ee1790fff30ddf62dc18d9533af1
rossneilson <rossneilson99@gmail.com>
2024-01-23 21:09:13 +0000

    Adding link to medium

--- app/page.tsx
+++ app/page.tsx
@@ -325,7 +325,14 @@ export default function Home() {
         This archive was put together to the best of the ability of The Internet
         Community Company.
         <br /> Information in this archive is provided as is for historical
-        reference and may not be accurate or complete.{" "}
+        reference and may not be accurate or complete. <br />
+        <a
+          href="https://medium.com/@ticc/a-catch-up-for-dxdao-members-on-ticc-302707366262"
+          target="_blank"
+          className="underline font-bold mt-10"
+        >
+          What is TICC?
+        </a>
       </p>
     </main>
   );
